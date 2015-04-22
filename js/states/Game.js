ff8308752b37a4b84b154a8ae8170c9c3b4e75e3
AmAuron <undefined.jrafael@gmail.com>
2015-04-22 06:13:08 +0100

    Ledges and ground created ready for test
    Testing ready

--- js/states/Game.js
+++ js/states/Game.js
@@ -3,5 +3,26 @@ PlatformGame.Game= function(){
 }
 
 PlatformGame.Game.prototype = {
+	create: function(){
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 
+		game.add.sprite( 0, 0, 'sky');
+
+		platforms = game.add.group();
+
+		platforms.enableBody = true;
+
+		var ground = platforms.create(0, game.world.height - 64, 'platform');
+
+		ground.body.immovable = true;
+
+		var ledges = platform.create(400, 400, 'platform');
+
+		ledge.body.immovable = true;
+
+		ledge = platforms.create(-150, 250, 'platform');
+
+		ledge.body.immovable = true;
+
+	}
 };
