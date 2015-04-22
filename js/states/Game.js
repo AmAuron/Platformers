03298bdfcfbe267034e397197934178c34871643
AmAuron <undefined.jrafael@gmail.com>
2015-04-22 06:20:48 +0100

    Fixes and add player

--- js/states/Game.js
+++ js/states/Game.js
@@ -4,25 +4,41 @@ PlatformGame.Game= function(){
 
 PlatformGame.Game.prototype = {
 	create: function(){
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		game.add.sprite( 0, 0, 'sky');
+		//begin sky creation
+		this.game.add.sprite( 0, 0, 'sky');
 
-		platforms = game.add.group();
+		//begin platforms creation
+		this.platforms = this.game.add.group();
 
-		platforms.enableBody = true;
+		this.platforms.enableBody = true;
 
-		var ground = platforms.create(0, game.world.height - 64, 'platform');
+		var ground = this.platforms.create(0, game.world.height - 64, 'platform');
 
-		ground.body.immovable = true;
+		this.ground.body.immovable = true;
 
-		var ledges = platform.create(400, 400, 'platform');
+		var ledges = this.platform.create(400, 400, 'platform');
 
-		ledge.body.immovable = true;
+		this.ledge.body.immovable = true;
 
-		ledge = platforms.create(-150, 250, 'platform');
+		this.ledge = platforms.create(-150, 250, 'platform');
 
-		ledge.body.immovable = true;
+		this.ledge.body.immovable = true;
 
+		this.player = this.game.add.sprite(32, game.world.height - 150, 'dude');
+
+		this.game.physics.arcade.enable(player);
+
+		this.player.body.bounce.y = 0.2;
+		this.player.body.gravity.y = 300;
+		this.player.body.collideWorldBounds = true;
+
+		this.player.animations.add('left', [0,1,2,3], 10, true);
+		this.player.animations.add('right', [5,6,7,8], 10, true);
+	},
+
+	update: function(){
+		this.game.physics.arcade.collide(player, platforms);
 	}
 };
