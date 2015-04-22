c256eb1c8ae6f2cf2f56db78a264bf88b42695d2
AmAuron <undefined.jrafael@gmail.com>
2015-04-22 03:35:07 +0100

    Adding Boot
    Boot and images added

---
+++ js/states/Boot.js
@@ -0,0 +1,33 @@
+var PlatformGame = function(){};
+
+PlatformGame.Boot= function(){};
+
+PlatformGame.Boot.prototype = {
+
+	preload: function(){
+		this.load.image('logo', 'assets/images/logo.png');
+		this.load.image('preloaderBar', 'assets/images/progressbar.png');
+	},
+
+	create: function(){
+		this.game.stage.backgroundColor = '#000000';
+
+		this.input.maxPointers = 1;
+
+		if(this.game.device.desktop){
+			this.scale.pageAlignHorizontally = true;
+		}
+		else{
+			  this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		      this.scale.minWidth = 568;
+		      this.scale.minHeight = 600;
+		      this.scale.maxWidth = 2048;
+		      this.scale.maxHeight = 1536;
+		      this.scale.forceLandscape = true;
+		      this.scale.pageAlignHorizontally = true;
+		      this.scale.setScreenSize(true);
+		}
+
+		this.state.start('Preloader');
+	}
+}
