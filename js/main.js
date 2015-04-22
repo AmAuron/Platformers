6395045681e17cb6f54e22e5d6441008c77d1b11
AmAuron <undefined.jrafael@gmail.com>
2015-04-22 06:27:25 +0100

    First Test
    first test incomming

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '
 
 game.state.add('Boot', PlatformGame.Boot);
 game.state.add('Preloader', PlatformGame.Preload);
+game.state.add('Game', PlatformGame.Game);
 
 game.state.start('Boot');
